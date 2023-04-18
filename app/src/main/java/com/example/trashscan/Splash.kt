031a1b839023a7d8b19652b53723cfb2ed7d0344
addy0110 <addycoder0110@gmail.com>
2023-04-18 18:35:21 +0530

    Added splash screen

---
+++ app/src/main/java/com/example/trashscan/Splash.kt
@@ -0,0 +1,18 @@
+package com.example.trashscan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
+    }
+}
