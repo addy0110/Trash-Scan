53cae9f1a7b4b3cd0da98ed2a60a6485234070b5
addy0110 <addycoder0110@gmail.com>
2023-05-02 00:14:26 +0530

    Added choose from camera

--- app/src/main/java/com/example/trashscan/MainPageFragment.kt
+++ app/src/main/java/com/example/trashscan/MainPageFragment.kt
@@ -1,21 +1,23 @@
 package com.example.trashscan
 
+import android.content.ContentValues
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.media.ThumbnailUtils
-import android.opengl.Visibility
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.provider.MediaStore
-import android.text.Layout.Directions
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.trashscan.databinding.FragmentMainPageBinding
 import com.example.trashscan.ml.Model
@@ -25,12 +27,16 @@ import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.IOException
 import java.nio.ByteBuffer
 
+
 class MainPageFragment : Fragment() {
+    private var imageUri: Uri? = null
     lateinit var binding: FragmentMainPageBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d("@@camera","main")
+
         // Inflate the layout for this fragment
         binding = FragmentMainPageBinding.inflate(layoutInflater,container,false)
         binding.pbProgressBar.visibility = View.GONE
@@ -42,17 +48,51 @@ class MainPageFragment : Fragment() {
     }
 
     private fun pickImage() {
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 100)
+        val options = arrayOf<CharSequence>("Take Photo", "Choose From Gallery", "Cancel")
+//        val builder: android.support.v7.app.AlertDialog.Builder = Builder(activity)
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Select Option")
+        builder.setItems(options, DialogInterface.OnClickListener { dialog, item ->
+            if (options[item] == "Take Photo") {
+                dialog.dismiss()
+                Log.d("@@camera","take photo")
+                val fileName = "new-photo-name.jpg"
+                val values = ContentValues()
+                values.put(MediaStore.Images.Media.TITLE, fileName)
+                values.put(MediaStore.Images.Media.DESCRIPTION, "Image capture by camera")
+                imageUri = requireContext().getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    values
+                )
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, 100)
+            } else if (options[item] == "Choose From Gallery") {
+                dialog.dismiss()
+                val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhoto, 200)
+            } else if (options[item] == "Cancel") {
+                dialog.dismiss()
+            }
+        })
+        builder.show()
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 100 && resultCode == AppCompatActivity.RESULT_OK && data != null) {
+        if (requestCode == 200 && resultCode == AppCompatActivity.RESULT_OK && data != null) {
             val imageBitmap = data.data!!
             var bitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(), imageBitmap)
             bitmap = ThumbnailUtils.extractThumbnail(bitmap,150,150)
             classifyImage(bitmap)
+        }
+        else if(requestCode == 100 && resultCode == AppCompatActivity.RESULT_OK){
+            Log.d("@@camera","into")
+                Log.d("@@camera","here")
+                var bitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(), imageUri)
+                bitmap = ThumbnailUtils.extractThumbnail(bitmap,150,150)
+                classifyImage(bitmap)
+        }
+        else{
+            Log.d("@@camera","data null")
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
